261358e932be14abafb53aa1d1095754b7b06b50
matan <0aroundtheworld1@gmail.com>
2021-12-30 22:02:10 +0200

    finished with game logic

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,56 +1,71 @@
 const options = document.querySelectorAll(".options");
-
+let pScore = 0;
+let cScore = 0;
+let gameScore = pScore + cScore ;
 options.forEach(option=>{
     option.addEventListener("click" , function(){
         const pInput = this.textContent;
 
         const cOptions = ["Rock", "Paper", "Scissors"];
+// computer choice
         const cInput = cOptions[Math.floor(Math.random()* 3 ) ];
         console.log(cInput);
 
         compareInputs(pInput, cInput);
 
+        
+
+
+function compareInputs (pInput, cInput){
+   
+
+    const currentMatch = '${pInput} vs ${cInput}';
+    // tie check
+        if (pInput === cInput){
+            alert ('${currentMatch} is a tie!');
+            return;
+        }
+    // rock win check
+    else if (pInput === 'Rock') {
+        if (cInput === 'Scissors') {
+            alert ('${currentMatch} = You win!');
+            pScore ++;
+        } else {
+            alert ('${currentMatch} = You lost');
+            cScore ++;
+        }
+    } 
+    // paper win check
+    else if (pInput === 'Paper'){
+        if (cInput === 'Rock'){
+            alert ('${currentMatch} = You win!');
+            pScore ++;
+        } else {
+            alert ('${currentMatch} = You lost');
+            cScore ++;
+        }
+    }
+    //scissors win check
+    else if (pInput === 'Scissors'){
+        if (cInput === 'Paper'){
+            alert ('${currentMatch} = You win!');
+            pScore++;
+        } else {
+            alert ('${currentMatch} = You lost');
+            cScore ++;
+        }
+    }
+    }
+    console.log(cScore);
+    console.log(pScore);
+    console.log(gameScore);
+    
     })
 }
 
 )
 
-function compareInputs (pInput, cInput){
 
-const currentMatch = '${pInput} vs ${cInput}';
-// tie check
-    if (pInput === cInput){
-        alert ('${currentMatch} is a tie!');
-        return;
-    }
-// rock win check
-else if (pInput === 'Rock') {
-    if (cInput === 'Scissors') {
-        alert ('${currentMatch} = You win!');
-    } else {
-        alert ('${currentMatch} = You lost');
-    }
-} 
-// paper win check
-else if (pInput === 'Paper'){
-    if (cInput === 'Rock'){
-        alert ('${currentMatch} = You win!');
-    } else {
-        alert ('${currentMatch} = You lost');
-    }
-}
-//scissors win check
-else if (pInput === 'Scissors'){
-    if (cInput === 'Paper'){
-        alert ('${currentMatch} = You win!');
-    } else {
-        alert ('${currentMatch} = You lost');
-    }
-}
-}
-
-console.log(compareInputs());
-
-
+co
 
 
