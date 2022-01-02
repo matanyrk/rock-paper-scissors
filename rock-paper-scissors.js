7fa4b8d520654369e1a1e535869ba9f72eef9994
matan <0aroundtheworld1@gmail.com>
2022-01-02 20:30:25 +0200

    added a scoreboard feature, allows keeping track of player wins, computer wins, and ties

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,9 +1,12 @@
 const options = document.querySelectorAll(".options");
 let pScore = 0;
 let cScore = 0;
+let tieScore = 0;
 let gameScore = pScore + cScore ;
 options.forEach(option=>{
     option.addEventListener("click" , function(){
+        let gameScore = pScore + cScore + tieScore;
+        gameScore ++;
         const pInput = this.textContent;
 
         const cOptions = ["Rock", "Paper", "Scissors"];
@@ -13,48 +16,53 @@ options.forEach(option=>{
 
         compareInputs(pInput, cInput);
 
-        
-
-
+// game logic
 function compareInputs (pInput, cInput){
-   
 
     const currentMatch = '${pInput} vs ${cInput}';
     // tie check
         if (pInput === cInput){
-            alert ('${currentMatch} is a tie!');
-            return;
+            alert ('It\'s a tie');
+            tieScore ++;
         }
     // rock win check
     else if (pInput === 'Rock') {
         if (cInput === 'Scissors') {
-            alert ('${currentMatch} = You win!');
+            alert ('You win!');
             pScore ++;
         } else {
-            alert ('${currentMatch} = You lost');
+            alert ('You lost');
             cScore ++;
         }
     } 
     // paper win check
     else if (pInput === 'Paper'){
         if (cInput === 'Rock'){
-            alert ('${currentMatch} = You win!');
+            alert ('You win!');
             pScore ++;
         } else {
-            alert ('${currentMatch} = You lost');
+            alert ('You lost');
             cScore ++;
         }
     }
     //scissors win check
     else if (pInput === 'Scissors'){
         if (cInput === 'Paper'){
-            alert ('${currentMatch} = You win!');
+            alert ('You win!');
             pScore++;
         } else {
-            alert ('${currentMatch} = You lost');
+            alert ('You lost');
             cScore ++;
         }
     }
+    document.querySelector("#gameScore").textContent = gameScore;
+    localStorage.setItem('gameScore', gameScore);
+    document.querySelector("#pScore").textContent = pScore;
+    localStorage.setItem('pScore', pScore);
+    document.querySelector("#cScore").textContent = cScore;
+    localStorage.setItem('cScore', cScore);
+    document.querySelector("#tieScore").textContent = tieScore;
+    localStorage.setItem('tieScore', tieScore);
     }
     console.log(cScore);
     console.log(pScore);
@@ -66,6 +74,6 @@ function compareInputs (pInput, cInput){
 )
 
 
-co
+
 
 
