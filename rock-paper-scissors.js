3e2aa6d8599decaf8349f40776b185157e7af646
matan <0aroundtheworld1@gmail.com>
2022-01-03 22:27:03 +0200

    added a round system

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -3,8 +3,12 @@ let pScore = 0;
 let cScore = 0;
 let tieScore = 0;
 let gameScore = pScore + cScore ;
+let pRounds = 0;
+let cRounds = 0;
 options.forEach(option=>{
     option.addEventListener("click" , function(){
+        
+        
         let gameScore = pScore + cScore + tieScore;
         gameScore ++;
         const pInput = this.textContent;
@@ -22,39 +26,62 @@ function compareInputs (pInput, cInput){
     const currentMatch = '${pInput} vs ${cInput}';
     // tie check
         if (pInput === cInput){
-            alert ('It\'s a tie');
+            alert ('It\'s a tie!');
             tieScore ++;
         }
     // rock win check
     else if (pInput === 'Rock') {
         if (cInput === 'Scissors') {
-            alert ('You win!');
+            alert (`You win! ${pInput} beats ${cInput}`)
             pScore ++;
         } else {
-            alert ('You lost');
+            alert (`You lost. ${cInput} beats ${pInput}`);
             cScore ++;
         }
     } 
     // paper win check
     else if (pInput === 'Paper'){
         if (cInput === 'Rock'){
-            alert ('You win!');
+            alert (`You win! ${pInput} beats ${cInput}`);
             pScore ++;
         } else {
-            alert ('You lost');
+            alert (`You lost. ${cInput} beats ${pInput}`);
             cScore ++;
         }
     }
     //scissors win check
     else if (pInput === 'Scissors'){
         if (cInput === 'Paper'){
-            alert ('You win!');
+            alert (`You win! ${pInput} beats ${cInput}`);
             pScore++;
         } else {
-            alert ('You lost');
+            alert (`You lost. ${cInput} beats ${pInput}`);
             cScore ++;
         }
     }
+}
+
+// reset score after player or cpu wins 5 games
+if (pScore === 5) {
+    alert ('Congratulations! You won!');
+    pScore = 0;
+    cScore = 0;
+    tieScore = 0;
+    gameScore = 0;
+    pRounds ++;
+}
+
+if (cScore === 5) {
+    alert ('The CPU won! Try again');
+    pScore = 0;
+    cScore = 0;
+    tieScore = 0;
+    gameScore = 0;
+    cRounds ++;
+}
+    
+    
+
     document.querySelector("#gameScore").textContent = gameScore;
     localStorage.setItem('gameScore', gameScore);
     document.querySelector("#pScore").textContent = pScore;
@@ -63,17 +90,18 @@ function compareInputs (pInput, cInput){
     localStorage.setItem('cScore', cScore);
     document.querySelector("#tieScore").textContent = tieScore;
     localStorage.setItem('tieScore', tieScore);
-    }
-    console.log(cScore);
-    console.log(pScore);
-    console.log(gameScore);
-    
-    })
+    document.querySelector("#pRounds").textContent = pRounds;
+    localStorage.setItem('pRounds', pRounds);
+    document.querySelector("#cRounds").textContent = cRounds;
+    localStorage.setItem('cScore', cScore);
+
 }
-
-)
-
+    )
 
 
 
 
+
+
+
+})
